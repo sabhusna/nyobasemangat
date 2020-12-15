2cebf1b2b0901548a3ec15d0d773c27e30f54ae7
sabhusna <76050968+sabhusna@users.noreply.github.com>
2020-12-15 20:53:55 +0700

    Add files via upload

---
+++ data/titikacuan_1.js
@@ -0,0 +1,8 @@
+var json_titikacuan_1 = {
+"type": "FeatureCollection",
+"name": "titikacuan_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 110.43695252466452, -7.055114661315922 ] } }
+]
+}
